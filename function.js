dc9351a6f3308b4b2574305ef5358e93f6b9664b
ocramgit <marstrabalhar@gmail.com>
2024-01-25 14:53:49 +0000

    first commit

---
+++ function.js
@@ -0,0 +1,32 @@
+const inputValue = document.getElementsByClassName("buttonValue");
+let inputChange = document.getElementsByClassName("inputChange");
+const multiplyValue = document.getElementsByClassName("multiplyValue");
+const resultValue = document.getElementsByClassName("result");
+
+let number = "";
+let operator = "*";
+
+for (let i = 0; i < inputValue.length; i++) {
+  inputValue[i].addEventListener("click", () => {
+    for (let j = 0; j < inputValue.length + 1; j++) {
+      inputChange[j].textContent += inputValue[i].value;
+    }
+    number += inputValue[i].value;
+    for (let k = 0; k < inputValue.length + 1; k++) {
+      let resultExp = number + operator + multiplyValue[k].textContent;
+      result[k].textContent = eval(resultExp);
+    }
+  });
+}
+
+const buttonValueField = document.getElementById("buttonValueField");
+const inputValueField = document.getElementById("inputValueField");
+
+buttonValueField.addEventListener("click", () => {
+  let numberField = inputValueField.value;
+  for (let i = 0; i < inputValue.length + 1; i++) {
+    inputChange[i].textContent = numberField;
+    let resultExp = numberField + operator + multiplyValue[i].textContent;
+    result[i].textContent = eval(resultExp);
+  }
+});
